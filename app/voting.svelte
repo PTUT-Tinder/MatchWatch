29476f6b356125983820c54c6a1f7e6756e979f3
ulyssecnt <76942440+ulyssecnt@users.noreply.github.com>
2021-03-08 14:15:36 +0100

    Merge branch 'main' of https://github.com/PTUT-Tinder/MatchWatch

--- app/voting.svelte
+++ app/voting.svelte
@@ -9,27 +9,7 @@
 
 <NavBar />
 
-<nav>
-  <div class="toggle">
-    <button class="hamburger hamburger--spin" type="button">
-      <span class="hamburger-box">
-        <span class="hamburger-inner" />
-      </span>
-    </button>
-  </div>
-  <ul id="nav-bar">
-    <li><a href="index.html"><img src={logo} alt="logoMatchWatch" /></a></li>
-    <div class="responsive-container">
-      <ul class="responsive">
-        <li><a href="#" id="joinSloon">Rejoindre un salon</a></li>
-        <li>
-          <a href="creation_salon.html" id="createSaloon">Créer un salon</a>
-        </li>
-        <li><a href="profile.html" id="profile">Mon profile</a></li>
-      </ul>
-    </div>
-  </ul>
-</nav>
+
 
 <main>
   <div class="container-films" />
