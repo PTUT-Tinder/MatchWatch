4e97e9912f5a4123942e31aed218c9ff584d1f6c
TopDresseurBalkany <66619116+Sok15@users.noreply.github.com>
2021-03-08 10:37:24 +0100

    on est la hein

--- app/js/create-room.js
+++ app/js/create-room.js
@@ -3,42 +3,23 @@ var btn_creer = document.getElementById("creer-salon");
 
 btn_creer.addEventListener("click", creerCode());
 
-function strRandom(o) {
-  var a = 10,
-      b = 'abcdefghijklmnopqrstuvwxyz',
-      c = '',
-      d = 0,
-      e = ''+b;
-  if (o) {
-    if (o.startsWithLowerCase) {
-      c = b[Math.floor(Math.random() * b.length)];
-      d = 1;
-    }
-    if (o.length) {
-      a = o.length;
-    }
-    if (o.includeUpperCase) {
-      e += b.toUpperCase();
-    }
-    if (o.includeNumbers) {
-      e += '1234567890';
-    }
-  }
-  for (; d < a; d++) {
-    c += e[Math.floor(Math.random() * e.length)];
-  }
-  return c;
+function makeid() {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (var i = 0; i < 6; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;
 }
 
-
-
 function creerCode(){
-  var code;
-  code = strRandom({
-    UpperCase: true,
-    includeNumbers: true,
-    length: 6,
-    startsWithLowerCase: false
-  });;
-  window.alert(code);
+	var code;
+	code = strRandom({
+		UpperCase: true,
+		includeNumbers: true,
+		length: 6,
+		startsWithLowerCase: false
+	});;
+	window.alert(code);
 }
