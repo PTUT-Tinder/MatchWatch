3fa3783941b29999e109cef3a912ed68513d46ed
TopDresseurBalkany <66619116+Sok15@users.noreply.github.com>
2021-03-08 10:05:11 +0100

    yolo

--- app/js/create-room.js
+++ app/js/create-room.js
@@ -1,6 +1,13 @@
+
+var btn_creer = document.getElementById("creer-salon");
+
+btn_creer.addEventListener("click", creerCode());
+
+
 strRandom({
     UpperCase: true,
     includeNumbers: false,
     length: 6,
     startsWithLowerCase: false
   });
+
