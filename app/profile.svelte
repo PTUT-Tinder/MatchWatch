3a7aeb1948a0b36163e78ae82616ae3b293ec947
TopDresseurBalkany <66619116+Sok15@users.noreply.github.com>
2021-03-08 16:47:45 +0100

    oui

--- app/profile.svelte
+++ app/profile.svelte
@@ -15,7 +15,46 @@
         <div class="text">
             <h1 class="pseudo">Julien Lacroix</h1>
             <h2 class="mail">mail@example.com</h2>
-            <h2 class="genres">Genres préférés :</h2>
+            
+            <div class="genres">
+                <ul>
+                    <li>
+                        <h2 class="genres">Genres préférés :</h2>
+                        <ul>
+                            <li>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="action"
+                                        name="action"
+                                    />
+                                    <label for="action">Action</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="aventure"
+                                        name="aventure"
+                                    />
+                                    <label for="aventure">Aventure</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        id="thriller"
+                                        name="thriller"
+                                    />
+                                    <label for="thriller">Thriller</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 
@@ -28,6 +67,4 @@
     </div>
 </main>
 
-<footer>
-
-</footer>
+<footer />
